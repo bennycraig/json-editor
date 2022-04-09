0ab28ff0880513c6dbfd2a750a859d924f6d89ea
Benny Craig <bencraig@umich.edu>
2022-04-09 16:50:53 -0400

    Clone of original json-editor repository -- plus a commented out line

--- src/core.js
+++ src/core.js
@@ -13,6 +13,7 @@ import styleRules from './style.css.js'
 
 export class JSONEditor {
   constructor (element, options = {}) {
+    // console.log('JSONEditor object constructor call!')
     if (!(element instanceof Element)) throw new Error('element should be an instance of Element')
 
     this.element = element
